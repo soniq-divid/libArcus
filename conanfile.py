db2b6fe5ac5119ec49daec20e39be38bf897ae01
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-06-13 13:29:27 +0200

    back to new updates from ultimaker

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,12 @@
 import os
 
-from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
-from conan.tools.files.packager import AutoPackager
-from conans import ConanFile, tools
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools import files
+from conan import ConanFile
+from conans import tools
 
-required_conan_version = ">=1.44.1"
+required_conan_version = ">=1.46.2"
+
 
 class ArcusConan(ConanFile):
     name = "arcus"
@@ -17,16 +19,14 @@ class ArcusConan(ConanFile):
     revision_mode = "scm"
     exports = "LICENSE*"
     options = {
-        "build_python": [True, False],  # TODO: Fix the Sip recipe first in the https://github.com/ultimaker/conan-ultimaker-index.git
+        "build_python": [True, False],
         "shared": [True, False],
-        "fPIC": [True, False],
-        "examples": [True, False]
+        "fPIC": [True, False]
     }
     default_options = {
-        "build_python": False,
+        "build_python": True,
         "shared": True,
         "fPIC": True,
-        "examples": False
     }
     scm = {
         "type": "git",
@@ -35,20 +35,23 @@ class ArcusConan(ConanFile):
         "revision": "auto"
     }
 
-    @property
-    def _site_packages(self):
-        return "site-packages"
+    def build_requirements(self):
+        self.tool_requires("ninja/[>=1.10.0]")
+        self.tool_requires("cmake/[>=3.23.0]")
 
     def requirements(self):
-        # TODO: Add the Python and SIP requirements. First get it up and running for CuraEngine CI/CT
         self.requires("protobuf/3.17.1")
 
+    def system_requirements(self):
+        pass  # Add Python here ???
+
     def config_options(self):
-        self.options["protobuf"].shared = self.options.shared
+        if self.options.shared and self.settings.compiler == "Visual Studio":
+            del self.options.fPIC
+            self.options.shared = False
 
     def configure(self):
-        if self.options.shared or self.settings.compiler == "Visual Studio":
-            del self.options.fPIC
+        self.options["protobuf"].shared = self.options.shared
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
@@ -56,12 +59,9 @@ class ArcusConan(ConanFile):
 
     def generate(self):
         cmake = CMakeDeps(self)
-        cmake.build_context_activated = ["protobuf"]
-        cmake.build_context_build_modules = ["protobuf"]
-        cmake.build_context_suffix = {"protobuf": "_BUILD"}
         cmake.generate()
 
-        tc = CMakeToolchain(self)
+        tc = CMakeToolchain(self, generator = "Ninja")
 
         if self.settings.compiler == "Visual Studio":
             tc.blocks["generic_system"].values["generator_platform"] = None
@@ -69,7 +69,48 @@ class ArcusConan(ConanFile):
 
         tc.variables["ALLOW_IN_SOURCE_BUILD"] = True
         tc.variables["BUILD_PYTHON"] = self.options.build_python
+        if self.options.build_python:
+            tc.variables["Python_VERSION"] = "3.10.4"
+            if self.options.shared and self.settings.os == "Windows":
+                tc.variables["Python_SITELIB_LOCAL"] = self.cpp.build.bindirs[0]
+            else:
+                tc.variables["Python_SITELIB_LOCAL"] = self.cpp.build.libdirs[0]
+
         tc.generate()
+
+    def layout(self):
+        cmake_layout(self)
+
+        # libarcus component
+        self.cpp.source.components["libarcus"].includedirs = ["arcus_include"]
+
+        self.cpp.build.components["libarcus"].libs = ["Arcus"]
+        self.cpp.build.components["libarcus"].libdirs = ["."]
+
+        self.cpp.package.components["libarcus"].includedirs = ["arcus_include"]
+        self.cpp.package.components["libarcus"].libs = ["Arcus"]
+        self.cpp.package.components["libarcus"].libdirs = ["lib"]
+        self.cpp.package.components["libarcus"].requires = ["protobuf::protobuf"]
+        self.cpp.package.components["libarcus"].defines = ["ARCUS"]
+        if self.settings.build_type == "Debug":
+            self.cpp.package.components["libarcus"].defines.append("ARCUS_DEBUG")
+        if self.settings.os in ["Linux", "FreeBSD", "Macos"]:
+            self.cpp.package.components["libarcus"].system_libs = ["pthread"]
+        elif self.settings.os == "Windows":
+            self.cpp.package.components["libarcus"].system_libs = ["ws2_32"]
+
+        # pyarcus component
+        if self.options.build_python:
+            self.cpp.source.components["pyarcus"].includedirs = ["pyarcus_include"]
+
+            self.cpp.build.components["pyarcus"].libdirs = [".", os.path.join("pyArcus", "pyArcus")]
+
+            self.cpp.package.components["pyarcus"].includedirs = ["pyarcus_include"]
+            self.cpp.package.components["pyarcus"].libdirs = ["site-packages"]
+            self.cpp.package.components["pyarcus"].requires = ["libarcus", "protobuf::protobuf"]
+            self.cpp.package.components["pyarcus"].system_libs = ["Python3.10"]
+            if self.settings.os in ["Linux", "FreeBSD", "Macos"]:
+                self.cpp.package.components["pyarcus"].system_libs.append("pthread")
 
     def build(self):
         cmake = CMake(self)
@@ -77,23 +118,24 @@ class ArcusConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*Arcus.*", dst="lib", src=self.build_folder, excludes=("python/*", "CMakeFiles/*"))
-        self.copy("*.h", dst="include/Arcus", src=f"{self.source_folder}/src", excludes=("./PlatformSocket_p.h", "./Socket_p.h", "./WireMessage_p.h"))
-        self.copy("*.h", dst="include/Arcus", src=f"{self.build_folder}/src")
+        packager = files.AutoPackager(self)
+        packager.patterns.build.lib = ["*.so", "*.so.*", "*.a", "*.lib", "*.dylib", "*.pyd", "*.pyi"]
+        packager.run()
 
+        # Workaround for AutoPackager not playing nice with components
+        files.rmdir(self, os.path.join(self.package_folder, self.cpp.package.components["libarcus"].libdirs[0], "CMakeFiles"))
+        tools.remove_files_by_mask(os.path.join(self.package_folder, self.cpp.package.components["libarcus"].libdirs[0]), "pyArcus.*")
+        files.rmdir(self, os.path.join(self.package_folder, self.cpp.package.components["libarcus"].libdirs[0], "pyArcus"))
+        if self.options.build_python:
+            files.rmdir(self, os.path.join(self.package_folder, self.cpp.package.components["pyarcus"].libdirs[0], "CMakeFiles"))
+            tools.remove_files_by_mask(os.path.join(self.package_folder, self.cpp.package.components["libarcus"].libdirs[0], "pyArcus"), "pyArcus.*")
+            files.rmdir(self, os.path.join(self.package_folder, self.cpp.package.components["pyarcus"].libdirs[0], "pyArcus"))
+            tools.remove_files_by_mask(os.path.join(self.package_folder, self.cpp.package.components["pyarcus"].libdirs[0]), "Arcus.*")
+            tools.remove_files_by_mask(os.path.join(self.package_folder, self.cpp.package.components["pyarcus"].libdirs[0]), "libArcus.*")
 
     def package_info(self):
-        # To stay compatible with the FindArcus module. This should be removed when we fully switch to Conan
-        self.cpp_info.set_property("cmake_file_name", "Arcus")
-        self.cpp_info.set_property("cmake_target_aliases", ["Arcus"])
-
-        self.cpp_info.libs = ["Arcus"]
-        self.cpp_info.defines.append("ARCUS")
-        if self.settings.build_type == "Debug":
-            self.cpp_info.defines.append("ARCUS_DEBUG")
-        if self.settings.os in ["Linux", "FreeBSD", "Macos"]:
-            self.cpp_info.system_libs.append("pthread")
-        elif self.settings.os == "Windows":
-            self.cpp_info.system_libs.append("ws2_32")
         if self.options.build_python:
-            self.runenv_info.append("PYTHONPATH", os.path.join(self.package_folder, self._site_packages))
+            if self.in_local_cache:
+                self.runenv_info.append_path("PYTHONPATH", self.cpp_info.components["pyarcus"].libdirs[0])
+            else:
+                self.runenv_info.append_path("PYTHONPATH", self.cpp_info.components["pyarcus"].libdirs[0])
